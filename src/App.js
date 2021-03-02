629d2a7975aff00dbe4e72e4f58820a00055b712
Yuwen Memon <yuwenis@gmail.com>
2021-03-01 19:58:29 -0800

    Hello world app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import yuwen from './yuwen.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={yuwen} className="App-logo" alt="logo" />
+        <p>
+          Hello World!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
